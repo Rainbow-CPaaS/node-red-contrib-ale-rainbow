c1d75fafd06f2c24df20bbd5ddb1a5b79a684836
vberder <vincent.berder@al-enterprise.com>
2022-06-17 10:44:43 +0200

    Jenkinsfile-prod

--- Jenkinsfile-prod.groovy
+++ Jenkinsfile-prod.groovy
@@ -264,7 +264,7 @@ pipeline {
                 withCredentials([sshUserPrivateKey(credentialsId: 'c75fd541-3fca-4399-b551-ab8288126dec', keyFileVariable: 'SSH_KEY')]) {
                     sh 'set -x '
                     sh 'echo  SSH_KEY : $SSH_KEY > ressshkey.txt'
-                    sh 'more ressshkey.txt |grep -i ssh'
+                    sh "bash -xc 'more ressshkey.txt |grep -i ssh'"
                     sh 'echo ssh -i $SSH_KEY -l git -o StrictHostKeyChecking=no \\"\\$@\\" > local_ssh.sh'
                     sh 'chmod +x local_ssh.sh'
                     withEnv(['GIT_SSH=./local_ssh.sh']) {
